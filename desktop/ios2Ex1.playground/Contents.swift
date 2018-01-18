5b372c1447ca7e33c06bb415728aba57ed919431
Erin Dougher <edougher@students.mchenry.edu>
2018-01-18 08:29:16 -0600

    First Commit

---
+++ desktop/ios2Ex1.playground/Contents.swift
@@ -0,0 +1,31 @@
+
+
+let one = 23
+let two = 34
+let three = 45
+let four = 56
+let five = 67
+
+var six = 32
+var seven = 43
+var eight = 54
+var nine = 65
+var ten = 76
+
+var myArray = [one, two, three, four, five, six, seven, eight, nine, ten]
+
+myArray.sort()
+
+
+var newDictionary: [Int: Int] = [1: one, 2:two, 3:three, 4:four, 5:five, 6:six, 7:seven, 8:eight, 9:nine, 10:ten]
+
+print([1:one, 10:ten])
+
+newDictionary[10] = 99
+
+newDictionary[10]
+
+print (newDictionary[10]!)
+
+
+
